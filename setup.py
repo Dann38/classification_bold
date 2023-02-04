5a1561058cdcefe72db9b6ec8f2b08e0e053215a
Даниил Копылов <it-daniil@yandex.ru>
2023-02-04 20:43:15 +0800

    made the project structure

--- setup.py
+++ setup.py
@@ -12,11 +12,6 @@ setuptools.setup(
                 "by which you can judge whether it is bold or not",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.9"
